2a135781847205a97c31e0140b0d690f04cbf296
karim tarek <karimtarekabdelaziz@gmail.com>
2021-01-04 21:21:51 +0200

    update project 4/1

--- src/js/main.js
+++ src/js/main.js
@@ -32,28 +32,7 @@ jQuery(function () {
     } else {
       $("html").attr({ dir: "ltr", lang: "en" });
     }
-    // Form
-    // $("form")
-    //   .find(`[key]`)
-    //   .each(function (i, cur) {
-    //     //
-    //     if ($(cur).is("input, textarea")) {
-    //       $(cur).attr(
-    //         "placeholder",
-    //         localizition.pages[$(this).attr("key")]["form"][
-    //           $(this).data("lang")
-    //         ]
-    //       );
-    //     }
-    //     //
-    //     if ($(cur).is("label, button")) {
-    //       $(cur).text(
-    //         localizition.pages[$(this).attr("key")]["form"][
-    //           $(this).data("lang")
-    //         ]
-    //       );
-    //     }
-    //   });
+
     // Navbar links
     // $(".navbar-nav a").each((i, cur) =>
     //   $(cur).text(localizition.navbar[$(cur).attr("key")])
@@ -87,67 +66,81 @@ jQuery(function () {
     renderLanguage(getLang);
     changeSelectLang(getLang);
   }
-  // Change dir slider
-  //   $(".slick-slider").addClass("direction-ltr");
-  //
-  //   $("footer #date").text(new Date().getFullYear());
-  // Lazy loading image
-  // All images
-  //   const allImages = $("[data-src]");
-  // Preload image
-  //   function preloadImages(img) {
-  //     const src = $(img).data("src");
-  //     if (!src) return;
-  //     img.src = src;
-  //     $(img).removeAttr("data-src");
-  //   }
-  //   //
-  //   const ImageObServer = new IntersectionObserver(
-  //     (entries, ImageObServer) => {
-  //       $(entries).each((i, cur) => {
-  //         if (!cur.isIntersecting) {
-  //           cur.target.classList.add("fadeOut");
-  //           return;
-  //         } else {
-  //           //
-  //           preloadImages(cur.target);
-  //           cur.target.classList.remove("fadeOut");
-  //           cur.target.classList.add("fadeIn");
-  //           ImageObServer.unobserve(cur.target);
-  //         }
-  //       });
-  //     },
-  //     {
-  //       threshold: 0,
-  //     }
-  //   );
-  //   //
-  //   allImages.each((i, cur) => ImageObServer.observe(cur));
-});
 
-// Header slider
-// $(".header__slider, .testimonial").slick({
-//   infinite: false,
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   autoplay: true,
-//   autoplaySpeed: 5000,
-//   arrows: true,
-//   prevArrow: `<svg class='a-left control-c prev slick-prev'>
-//     <use xlink:href="../icons/sprite.svg#icon-Angle-Left">
-//     </svg>`,
-//   nextArrow: `<svg class='a-right control-c next slick-next'>
-//     <use xlink:href="../icons/sprite.svg#icon-Angle-Right">
-//     </svg>`,
-//   responsive: [
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         dots: false,
-//         arrows: false,
-//       },
-//     },
-//   ],
-// });
+  /////////////////////////////////////
+  ///// Section about us
+  // More Paragraph
+  $(".more").on("click", function (e) {
+    // 1) Prevent default anchor
+    e.preventDefault();
+    // 2) Toggle class more on paragraph
+    $(this).parent().children("p").toggleClass("more");
+    // 3) Check if pargarph has class more or not
+    if ($(this).parent().children("p").hasClass("more")) {
+      $("html").attr("dir") === "ltr"
+        ? $(this).text("less")
+        : $(this).text("اقل");
+    } else {
+      $("html").attr("dir") === "ltr"
+        ? $(this).text("view more")
+        : $(this).text("المزيــــد");
+    }
+  });
+
+  // Header slider
+  $(".our-projects__wrapper").slick({
+    lazyLoad: "progressive",
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    arrows: false,
+    dots: true,
+    fade: true,
+    cssEase: "linear",
+  });
+
+  /////////////////////////////////////
+  //// Section our projects
+  //
+  const elementSlider = {
+    wrapper: $(".our-projects__wrapper"),
+    head: $("#head"),
+    par: $("#par"),
+  };
+
+  // Async function render data slider
+  async function renderDataSlider(url) {
+    try {
+      // 1) Get data from api
+      const dataSlider = await $.get(url);
+
+      // 2) For each all
+      dataSlider.forEach((cur, index) => {
+        // 1) Render img tag, and include data
+        const image = `<img src=${cur.img} class="obj-image" alt=${cur.name} />`;
+        // 2) Append image in slider
+        elementSlider.wrapper.slick("slickAdd", image);
+        // 3) Get index contains class active and get it index
+        const indexActiveClass = $(".slick-dots li.slick-active").index();
+        // 4) If indes === index active class / will be render paragraph from this index
+        if (index === indexActiveClass) {
+          elementSlider.head.text(cur.name);
+          elementSlider.par.text(cur.par);
+        }
+      });
+
+      // 3) Get id after changed
+      elementSlider.wrapper.on("afterChange", function () {
+        var indexCurrentSlide = $(".slick-current").attr("data-slick-index");
+        elementSlider.head.text(dataSlider[indexCurrentSlide].name);
+        elementSlider.par.text(dataSlider[indexCurrentSlide].par);
+      });
+    } catch (err) {
+      alert("some error please try reload");
+    }
+  }
+  // Run fn render data slider
+  renderDataSlider("../localizition/api.json");
+});
